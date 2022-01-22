3911a939176ce6c1b1401b3324f1e21030398945
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2022-01-23 01:45:56 +0800

    Create Gdrive.js

---
+++ javascript/Gdrive.js
@@ -0,0 +1,65 @@
+(function($) {
+
+  $(function() {
+
+    var $shareLink = $('#sharelink'),
+
+      $downloadLink = $('#downloadlink'),
+
+      $copyButton = $('#copylinkbtn'),
+
+      clipboard;
+
+    $shareLink.on('keyup paste', function() {
+
+      var link = $shareLink.val(),
+
+        l = link.replace(/https:\/\/drive.google.com\/file\/d\/(.+)\/(.+)/, "https://docs.google.com/uc?export=download&id=$1");
+
+      
+
+      if(l !== link) {
+
+        $downloadLink.val(l);
+
+        $copyButton.removeAttr('disabled');
+
+      } else {
+
+        $downloadLink.val('');
+
+        $copyButton.attr('disabled', 'disabled');
+
+      }
+
+    });
+
+    $downloadLink.on('click', function() {
+
+      $downloadLink.select();
+
+    });
+
+    clipboard = new Clipboard('#copylinkbtn');
+
+    clipboard.on('success', function(e) {
+
+      $.notify({
+
+        icon: 'glyphicon glyphicon-ok-circle',
+
+        title: 'Link copied to clipboard:',
+
+        message: e.text,
+
+        url: e.text,
+
+        target: '_blank'
+
+      }, {
+
+        // settings
+
+        type: "success",
+
+        placement: {
