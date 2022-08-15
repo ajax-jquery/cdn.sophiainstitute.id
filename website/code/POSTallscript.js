28779fceead943578ef8360194790ecd207d4a59
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2022-08-15 17:09:56 +0800

    Create POSTallscript.js

---
+++ website/code/POSTallscript.js
@@ -0,0 +1,3 @@
+function PostM(f,e,i){'use strict';var h=window.document.createElement("script"),g=e||window.document.getElementsByTagName("script")[0];h.async=!0,h.src=f,g.parentNode.insertBefore(h,g),setTimeout(function(){h.media=i||"all"})}
+PostM('https://cdn.sophiainstitute.id/website/code/Permalinks-bar.min.js');
+PostM('https://cdn.jsdelivr.net/gh/ajax-jquery/cdn.sophiainstitute.id@master/website/code/slide-ebook.js');
